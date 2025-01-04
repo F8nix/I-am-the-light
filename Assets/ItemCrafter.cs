7b255a64bccae8bc628e0a6506df87d12cc7c87d
F8nix <kamilkonfederak@interia.pl>
2025-01-04 19:14:52 +0100

    ItemCrafter refactorization

--- Assets/ItemCrafter.cs
+++ Assets/ItemCrafter.cs
@@ -12,9 +12,6 @@ public class ItemCrafter : MonoBehaviour
 
     private RarityEnum craftedRarity;
 
-    private int prefixNumb;
-    private int suffixNumb;
-
     private int magicPrefixMin = 1;
     private int magicPrefixMax = 2;
     private int magicSuffixMin = 1;
@@ -24,10 +21,6 @@ public class ItemCrafter : MonoBehaviour
     private int rarePrefixMax = 3;
     private int rareSuffixMin = 2;
     private int rareSuffixMax = 3;
-
-    private (string, float) _implicit;
-    private List<(string, float)> prefixesList = new List<(string, float)>();
-    private List<(string, float)> suffixesList = new List<(string, float)>();
 
     [SerializeField] public List<ModifierData> modifiers = new List<ModifierData>();
     // Start is called before the first frame update
@@ -86,51 +79,40 @@ public class ItemCrafter : MonoBehaviour
         return modifiersList.Select(modifier => (modifier.modifierName, modifier.modifierStrength)).ToList();
     }
 
-    private void SetAffixesRange(RarityEnum rarity){
-        prefixNumb = 0;
-        suffixNumb = 0;
+    private (int, int) SetAffixesRange(RarityEnum rarity){
+        int prefixNumb = 0;
+        int suffixNumb = 0;
         
         if (rarity == RarityEnum.Normal){
-            prefixNumb = 0;
-            suffixNumb = 0;
+            return (0, 0);
         } else if(rarity == RarityEnum.Magic){
             prefixNumb = UnityEngine.Random.Range(magicPrefixMin, magicPrefixMax + 1);
             suffixNumb = UnityEngine.Random.Range(magicSuffixMin, magicSuffixMax + 1);
-        } else if(rarity == RarityEnum.Rare){
+            return (prefixNumb, suffixNumb);
+        } else { //rare
             prefixNumb = UnityEngine.Random.Range(rarePrefixMin, rarePrefixMax + 1);
             suffixNumb = UnityEngine.Random.Range(rareSuffixMin, rareSuffixMax + 1);
+            return (prefixNumb, suffixNumb);
         }
-        Debug.Log(prefixNumb + (" :pref suff: ") + suffixNumb);
+        //Debug.Log(prefixNumb + (" :pref suff: ") + suffixNumb);
     }
 
     public Item CraftItem() {
-        craftedRarity = RarityEnum.Rare;
+        craftedRarity = RarityEnum.Magic;
         //test
-        if(craftedRarity == RarityEnum.Normal){
-            SetAffixesRange(craftedRarity);
-            _implicit = GetRandomModifier(ModifierTypeEnum.Implicit);
-            Item i = new Item(craftedRarity, _implicit);
-            return i;
-        }
-        if(craftedRarity == RarityEnum.Magic){
-            SetAffixesRange(craftedRarity);
-            _implicit = GetRandomModifier(ModifierTypeEnum.Implicit);
-            prefixesList = GetRandomModifier(ModifierTypeEnum.Prefix, prefixNumb);
-            suffixesList = GetRandomModifier(ModifierTypeEnum.Suffix, suffixNumb);
-            Item i = new Item(craftedRarity, _implicit, prefixesList, suffixesList);
-            return i;
-        }
-        if(craftedRarity == RarityEnum.Rare){
-            SetAffixesRange(craftedRarity);
-            _implicit = GetRandomModifier(ModifierTypeEnum.Implicit);
-            prefixesList = GetRandomModifier(ModifierTypeEnum.Prefix, prefixNumb);
-            suffixesList = GetRandomModifier(ModifierTypeEnum.Suffix, suffixNumb);
-            Item i = new Item(craftedRarity, _implicit, prefixesList, suffixesList);
-            return i;
-        }
+        (int prefixesAmount, int suffixesAmount) = SetAffixesRange(craftedRarity);
+        List<(string, float)> prefixesList = GetRandomModifier(ModifierTypeEnum.Prefix, prefixesAmount);
+        List<(string, float)> suffixesList = GetRandomModifier(ModifierTypeEnum.Suffix, suffixesAmount);
+        Item i = new Item(craftedRarity, GetRandomModifier(ModifierTypeEnum.Implicit), prefixesList, suffixesList);
+        Debug.Log(i.rarity + " " + i._implicit.Item1 + " " + i._implicit.Item2);
+            for (int j = 0; j < i.prefixes.Count; j++){
+                Debug.Log("Prefix " + i.prefixes[j].Item1 + " " + i.prefixes[j].Item2);
+            }
+            for (int k = 0; k < i.suffixes.Count; k++){
+                Debug.Log("Suffix " + i.suffixes[k].Item1 + " " + i.suffixes[k].Item2);
+            }
+        return i;
 
-        return null;
-        //some fancy exception error
 
         /* item test
             Debug.Log(i.rarity + " " + i._implicit.Item1 + " " + i._implicit.Item2);
